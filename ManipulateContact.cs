dc0c0171743fecf52eb4354671dd002d7aaaac16
rounak <rounakkole@gmail.com>
2022-02-19 15:57:57 +0530

    [rounak] ADDED: create contact

---
+++ ManipulateContact.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    internal class ManipulateContact
+    {
+        List<ContactDetails> AddressBookList = new List<ContactDetails>();
+        public void AddingContact
+            (
+            string Name,
+            string Address,
+            string City,
+            string State,
+            string ZipCode,
+            string PhoneNumber,
+            string Email
+            )
+        {
+            AddressBookList.Add
+                (new ContactDetails()
+                {
+                    Name = Name,
+                    Address = Address,
+                    City = City,
+                    State = State,
+                    ZipCode = ZipCode,
+                    PhoneNumber = PhoneNumber,
+                    Email = Email
+                }
+            );
+            Console.WriteLine($"{Name} is added");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,7 +7,19 @@ namespace AddressBookSystem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("welcome to address book system");
+
+            ContactDetails detail = new ContactDetails();
+            ManipulateContact AddContact = new ManipulateContact();
+            AddContact.AddingContact(
+                detail.Name = Console.ReadLine(),
+                detail.Address = Console.ReadLine(),
+                detail.City = Console.ReadLine(),
+                detail.State = Console.ReadLine(),
+                detail.ZipCode = Console.ReadLine(),
+                detail.PhoneNumber = Console.ReadLine(),
+                detail.Email = Console.ReadLine()
+                );
         }
     }
 }
